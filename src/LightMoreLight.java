38b43a966f432b51abb83a4b764eab39f0aea533
darran <darran@Hellfire>
2012-08-17 01:07:44 +1000

    Update LightMoreLight challenge

--- src/LightMoreLight.java
+++ src/LightMoreLight.java
@@ -11,9 +11,9 @@ public class LightMoreLight {
     Scanner in = new Scanner(System.in);
     String line = in.nextLine();
     while(!line.equals("0")){
-      int number = Integer.parseInt(line);
+      double number = Math.sqrt(Integer.parseInt(line));
       
-      if(Math.floor(Math.sqrt(number)) == Math.sqrt(number)){
+      if(Math.floor(number) == number){
         System.out.println("yes");
       } else {
         System.out.println("no");
