0d02420cb5dff4d4e924cede49112245bdaca91b
darran <darran@Hellfire>
2012-08-31 23:45:17 +1000

    Rework MST problem based on Malcolm's feedback. Now, much, much faster...

--- src/SpanningTreeProblem.java
+++ src/SpanningTreeProblem.java
@@ -1,6 +1,6 @@
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -9,14 +9,20 @@ public class SpanningTreeProblem {
   static int vertexCount;
   static int edgeCount;
   static PriorityQueue<edge> edges;
+  static ArrayList<ArrayList<edge>> adjacencyList;
   static Boolean[] visited;
-  private final static String OUTPUT_STRING 
-          = "Test %d, minimum spanning tree weight = %d\n";
+  private final static String OUTPUT_STRING = "Test %d, minimum spanning tree weight = %d\n";
 
+  /**
+   * Main
+   */
   public static void main(String[] args) {
     new SpanningTreeProblem().run();
   }
 
+  /**
+   * Run interface to be called from main().
+   */
   public void run() {
     Scanner in = new Scanner(System.in);
     int caseCount = Integer.parseInt(in.nextLine());
@@ -28,59 +34,46 @@ public class SpanningTreeProblem {
       vertexCount = in.nextInt();
       edgeCount = in.nextInt();
 
-      // Construct our priority queue, and visited map.
-      edges = new PriorityQueue<edge>(edgeCount, new edge(0, 0, 0));
+      // Construct visited map and adjacency list.
       visited = new Boolean[vertexCount];
+      adjacencyList = new ArrayList<ArrayList<edge>>();
 
-      // Set all vertices to not visited.
+      // Create priority queue.
+      edge comp = new edge( 0, 0);
+			edges = new PriorityQueue<edge>(edgeCount, comp);
+
+      // Set all vertices to not visited and ensure Adjacency list has items.
       for (int n = 0; n < vertexCount; n++) {
         visited[n] = false;
+        adjacencyList.add(new ArrayList<edge>());
       }
 
-      // Read all edges into the priority queue.
+      // Read all edges into the adjacency list.
       while (edgeCount-- > 0) {
-        edges.add(new edge(in.nextInt(), in.nextInt(), in.nextInt()));
+        int source = in.nextInt();
+        int dest = in.nextInt();
+        int weight = in.nextInt();
+        adjacencyList.get(source).add(new edge(dest, weight));
+        adjacencyList.get(dest).add(new edge(source, weight));
       }
 
       // Set our source vertex to be visited.
-      visited[edges.peek().sourceVertex] = true;
+      visited[0] = true;
+
+      // Add all vertex 0 edges to the priority queue
+      addEdges(0);
 
       // While not visited all vertices, continue to loop
-      while (!visitedAllVertices()) {
-        LinkedList<edge> tmpEdges = new LinkedList<edge>();
-        int sourceVertex, destinationVertex;
+      while (!edges.isEmpty()) {
+        edge e = edges.poll();
 
-        edge minEdge = edges.poll();
-        Boolean useThisEdge = false;
+        // if vertex is not yet connected
+        if (visited[e.vertex] == false) {
 
-        // find min edge with one node in tree and one node not in tree
-        do {
-          sourceVertex = minEdge.sourceVertex;
-          destinationVertex = minEdge.destinationVertex;
-          
-          // check for source being visited and destination not visited
-          if (visited[sourceVertex] && !visited[destinationVertex]) {
-            useThisEdge = true;
-          } else if (!visited[sourceVertex] && visited[destinationVertex]) {
-            // check for other direction
-            int tmp = sourceVertex; // swap source and destination
-            sourceVertex = destinationVertex;
-            destinationVertex = tmp;
-            useThisEdge = true;
-          }
-
-          if (!useThisEdge) {
-            tmpEdges.add(minEdge); // store unused edges for later
-            minEdge = edges.poll(); // get next edge to try
-          }
-        } while (!useThisEdge);
-
-        minimumWeight += minEdge.weight;
-        visited[destinationVertex] = true;
-
-        // copy unused edges back to priority queue
-        for(edge e: tmpEdges){
-          edges.add(e);
+          // take edge and process all edges incident to this edge
+          // each edge is in priority queue only once
+          minimumWeight += e.weight;
+          addEdges(e.vertex);
         }
       }
 
@@ -88,25 +81,44 @@ public class SpanningTreeProblem {
     }
   }
 
-  public Boolean visitedAllVertices() {
-    for (Boolean v : visited) {
-      if (!v) {
-        return false;
+  /**
+   * Add edges of vertex to the priority queue, whose edge has not been visited.
+   *
+   * @param vertex
+   */
+  public void addEdges(int vertex) {
+    visited[vertex] = true;
+    for (edge e : adjacencyList.get(vertex)) {
+      // Add in edge, if neighbour has NOT been visited, to avoid cycles.
+      if (visited[e.vertex] == false) {
+        edges.add(e);
       }
     }
-    return true;
   }
 
+  /**
+   * Edge container class.
+   */
   public class edge implements Comparator<edge> {
 
-    public int sourceVertex, destinationVertex, weight;
+    /**
+     * destination vertex and weight.
+     */
+    public int vertex, weight;
 
-    public edge(int s, int d, int w) {
-      sourceVertex = s;
-      destinationVertex = d;
+    /**
+     * Constructor
+     * @param v The destination vertex
+     * @param w The weight of the edge.
+     */
+    public edge(int v, int w) {
+      vertex = v;
       weight = w;
     }
 
+    /**
+     * Compare function for priority queue.
+     */
     @Override
     public int compare(edge e1, edge e2) {
       if (e1.weight < e2.weight) {
